ee04a39e6c3255b2835675f9481dd77ccf1aa89c
Egomax4 <rijul.saigal@research.iiit.ac.in>
2026-02-11 11:08:36 +0000

    Problem1 performance

--- problem1/mapper.py
+++ problem1/mapper.py
@@ -3,11 +3,6 @@ import re
 def tokenize(document):
     return re.sub("[.,\":;?!()\t\n]"," ",document).lower().split()
 
-# def map(document,combine_queue):
-#     tokenized_doc = tokenize(document)[1:]
-#     # print(tokenized_doc)
-#     combine_queue.put([(i,1) for i in tokenized_doc])
-
 def map(file,combine_queue,file_lock):
     while True:
         file_lock.acquire()
@@ -18,8 +13,5 @@ def map(file,combine_queue,file_lock):
             break
         
         doc = doc.decode('utf-8')
-        # print(doc)
         tokenized_doc = tokenize(doc)[1:]
-        # print(tokenized_doc)
-        # exit()
         combine_queue.put([(i,1) for i in tokenized_doc])
--- problem1/time_test.py
+++ problem1/time_test.py
@@ -4,13 +4,17 @@ from mapper import map
 from reducer import reduce
 from combiner import combine
 from math import log
+import time
 
-Mapper_count = 3
-Combiner_count = 3
+Mapper_count = 2
+Combiner_count = 4
 Reducer_count = 2
 # ... (Keep your counts and imports) ...
 
 def run_map_reduce(input_path, output_path):
+    start_job = time.time()
+
+    init_start = time.time()
     manager = Manager()
     combine_queue = manager.Queue()
     reduce_queue = manager.Queue()
@@ -20,6 +24,7 @@ def run_map_reduce(input_path, output_path):
     file_lock = manager.Lock()
     page_count_lock = manager.Lock()
     queue_lock = manager.Lock()
+    init_end = time.time()
 
     # Open the input file
     with open(input_path, "rb", buffering=0) as f:
@@ -36,23 +41,36 @@ def run_map_reduce(input_path, output_path):
         # Start Mappers (passing the file object f)
         Mappers = [Process(target=map, args=(f, combine_queue, file_lock)) 
                    for _ in range(Mapper_count)]
+        
+        map_start = time.time()
         for m in Mappers: m.start()
+        reduce_start = time.time()
 
         # Wait for Mappers to finish
         for m in Mappers: m.join()
+        map_end = time.time()
 
         # Shutdown sequence
+
         for _ in range(Combiner_count): combine_queue.put(None)
         for c in Combiners: c.join()
 
         for _ in range(Reducer_count): reduce_queue.put(None)
         for r in Reducers: r.join()
+        reduce_end = time.time()
 
+        end_job = time.time()
     # Write results to the output file
     with open(output_path, "w") as out_f:
         for key in sorted(doc_freq.keys()):
             idf = log(num_docs.value / doc_freq[key])
             out_f.write(f"{key}\t{idf}\n")
+
+    with open("time_calcs.txt","w") as f:
+        f.write(f"Initialization: {init_end - init_start:.4f}s\n")
+        f.write(f"Mapper Time: {map_end - map_start:.4f}s\n")
+        f.write(f"Reducer/Shuffle Time: {reduce_end - reduce_start:.4f}s\n")
+        f.write(f"Total Time: {end_job - start_job:.4f}s\n")
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
